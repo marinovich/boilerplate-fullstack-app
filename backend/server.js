5e286017f2f9e3a44d2a7efe62c60cddc3eb91a5
Aliaksandr Marynovich <Aliaksandr_Marynovich@epam.com>
2019-09-04 13:50:36 +0300

    master/feature: add basic functionality

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+
+const API_PORT = 3001;
+const app = express();
+app.use(cors());
+const router = express.Router();
+
+// (optional) only made for logging and
+// bodyParser, parses the request body to be a readable json format
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(logger('dev'));
+
+// this is our get method
+// this method fetches all available data in our database
+router.get('/getData', (req, res) => {
+  res.json({ message: 'Hello, client!' });
+});
+
+// append /api for our http requests
+app.use('/api', router);
+
+// launch our backend into a port
+app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import axios from 'axios';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Hello
+      </div>
+    );
+  }
+}
+
+export default App;
