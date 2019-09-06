14d92cbb939ae101c0b2fd82890c12bb23462c2f
Aliaksandr Marynovich <Aliaksandr_Marynovich@epam.com>
2019-09-06 17:20:24 +0300

    Add react-router

--- client/index.js
+++ client/index.js
@@ -1,5 +1,31 @@
+import { AppContainer } from 'react-hot-loader';
+import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/App';
+import App from './components/App';
+import configureStore, { history } from './configureStore';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+import './index.css';
+
+const store = configureStore();
+
+const render = () => {
+  ReactDOM.render(
+    <AppContainer>
+      <Provider store={store}>
+        <App history={history} />
+      </Provider>
+    </AppContainer>,
+    document.getElementById('app')
+  )
+}
+
+render();
+
+// Hot reloading
+if (module.hot) {
+  // Reload components
+  module.hot.accept('./App', () => {
+    render()
+  })
+}
